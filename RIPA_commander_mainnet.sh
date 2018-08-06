dfb434de11084f31bc2bb46af654aae6bc91dbad
Edgar Goetzendorff <goetzendorff@cip.ifi.lmu.de>
2018-08-06 19:27:11 +0200

    Removes obsolete submenu option

--- RIPA_commander_mainnet.sh
+++ RIPA_commander_mainnet.sh
@@ -1260,7 +1260,7 @@ read_options(){
 
 read_sub_options(){
 	local choice1
-	read -p "          Enter choice [1 - 7]: " choice1
+	read -p "          Enter choice [1 - 6]: " choice1
 	case $choice1 in
 		1) subone ;;
 		2) subtwo ;;
@@ -1268,7 +1268,6 @@ read_sub_options(){
 		4) four ;;
 		5) subfive ;;
 		6) subsix ;;
-		7) seven ;;
 		0) init ;;
 		*) echo -e "$(red "             Incorrect option!")" && sleep 1
 	esac
